46dcbefc58ab5f6ed3358b7a1b13b25ea352f734
GeorgEdy <eduard.dinu94@yahoo.ro>
2015-12-15 18:28:07 +0200

    micro 4

---
+++ micro4/js/main.js
@@ -0,0 +1,14 @@
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+document.addEventListener("mousemove", function (event) {
+  console.log(event);
+  var clientX = event.clientX;
+  var clientY = event.clientY;
+
+  var hue =Math.round(clientX/width *360);
+  var saturation = Math.round(clientY/height *100) + "%";
+var hsl = "hsl(" +hue + "," + saturation +", 50%)"
+document.body.style.backgroundColor = hsl;
+  console.log(hsl);
+});
