b15863645a4d4e11b6b84334d6a06b51b34a568b
GeorgEdy <eduard.dinu94@yahoo.ro>
2015-12-18 15:35:02 +0200

    Micro 9

---
+++ micro9/js/main.js
@@ -0,0 +1,9 @@
+var addBtn = document.getElementById("add-btn");
+
+var createRow = function() {
+  console.log('create row invoked');
+};
+
+addBtn.addEventListener("click", function () {
+   createRow();
+});
