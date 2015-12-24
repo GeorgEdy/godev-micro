99bb200ecace95d9ade19a45fd8c8cbdd3317b61
GeorgEdy <eduard.dinu94@yahoo.ro>
2015-12-24 15:27:02 +0200

    Macro

--- Macro/js/main.js
+++ Macro/js/main.js
@@ -182,14 +182,16 @@ function initMap() {
 function geocodeAddress(geocoder, resultsMap) {
     var address = document.getElementById('city').value;
     geocoder.geocode({'address': address}, function (results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: resultsMap,
-                position: results[0].geometry.location
-            });
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
+        if (isValidCity(data)) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                resultsMap.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                    map: resultsMap,
+                    position: results[0].geometry.location
+                });
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
         }
     });
 }
