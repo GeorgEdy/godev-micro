df79c4839266cccb123d5b695cc807ec8f8a08f8
GeorgEdy <eduard.dinu94@yahoo.ro>
2016-01-05 19:43:14 +0200

    Progress bar

--- micro1/js/main2.js
+++
@@ -1,5 +0,0 @@
-var button = document.getElementById('change');
-button.addEventListener("click", function() {	
-	var progress = document.getElementById('prog');
-	progress.value= Math.floor(Math.random() * (100 - 0 + 1) + 0);
-});
